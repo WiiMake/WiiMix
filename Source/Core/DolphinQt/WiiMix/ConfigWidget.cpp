f2e4ed8894e1b6efb345d00c64f8d5e4319c1504
xanmankey <86136010+xanmankey@users.noreply.github.com>
2024-10-15 19:07:28 -0400

    temporary push

--- Source/Core/DolphinQt/WiiMix/ConfigWidget.cpp
+++ Source/Core/DolphinQt/WiiMix/ConfigWidget.cpp
@@ -10,6 +10,8 @@
 #include <QLineEdit>
 #include <QDialog>
 
+#include "Core/Config/MainSettings.h"
+
 #include "DolphinQt/WiiMix/Enums.h"
 #include "DolphinQt/WiiMix/ModesWidget.h"
 #include "DolphinQt/WiiMix/ConfigWidget.h"
@@ -26,31 +28,47 @@ void WiiMixConfigWidget::CreateLayout(WiiMixEnums::Mode mode) {
     QVBoxLayout* config_layout = new QVBoxLayout();
 
     if (mode == WiiMixEnums::Mode::BINGO) {
+        bool is_lockout = Config::Get(Config::WIIMIX_IS_LOCKOUT);
         m_bingo_button = new QRadioButton(tr("Bingo"));
-        m_bingo_button->setEnabled(true);
+        m_bingo_button->setChecked(!is_lockout);
         m_lockout_button = new QRadioButton(tr("Lockout"));
+        m_lockout_button->setChecked(is_lockout);
         QHBoxLayout* mode_layout = new QHBoxLayout();
         config_layout->addWidget(m_bingo_button);
         config_layout->addWidget(m_lockout_button);
         // config_layout->addLayout(mode_layout);
-
+        
+        // TODOx: make this more dynamic
         QLabel* card_size_label = new QLabel(tr("Card Size:"));
+        int card_size = Config::Get(Config::WIIMIX_CARD_SIZE);
         m_card_size = new QComboBox();
         m_card_size->addItem(QStringLiteral("3x3"));
         m_card_size->addItem(QStringLiteral("5x5"));
         m_card_size->addItem(QStringLiteral("7x7"));
+        if (card_size == 9) {
+            m_card_size->setCurrentIndex(0);
+        }
+        else if (card_size == 25) {
+            m_card_size->setCurrentIndex(1);
+        }
+        else if (card_size == 49) {
+            m_card_size->setCurrentIndex(2);
+        }
         config_layout->addWidget(card_size_label);
         config_layout->addWidget(m_card_size);
     }
     else if (mode == WiiMixEnums::Mode::SHUFFLE) {
+        int num_switches = Config::Get(Config::WIIMIX_NUMBER_OF_SWITCHES);
         QHBoxLayout* num_switches_layout = new QHBoxLayout();
 
         QLabel* num_switches_label = new QLabel(tr("Number of Switches:"));
         m_num_switches = new QLineEdit();
+        m_num_switches->setText(QString::number(num_switches));
         QIntValidator* int_validator = new QIntValidator(MIN_NUM_OBJECTIVES, MAX_NUM_OBJECTIVES, this);
         m_num_switches->setValidator(int_validator);
 
-        m_endless_mode = new QCheckBox(tr("Endless WiiMixEnums::Mode"));
+        m_endless_mode = new QCheckBox(tr("Endless Mode"));
+        m_endless_mode->setChecked(Config::Get(Config::WIIMIX_IS_ENDLESS));
 
         num_switches_layout->addWidget(m_num_switches);
         num_switches_layout->addWidget(m_endless_mode);
@@ -61,15 +79,18 @@ void WiiMixConfigWidget::CreateLayout(WiiMixEnums::Mode mode) {
         QLabel* min_switch_time_label = new QLabel(tr("Min Time Between Shuffles: 15"));
         m_min_time_between_switch = new QSlider(Qt::Horizontal);
         m_min_time_between_switch->setRange(DEFAULT_MIN_SWITCH_TIME, DEFAULT_MAX_SWITCH_TIME);
+        m_min_time_between_switch->setValue(Config::Get(Config::WIIMIX_MIN_TIME_BETWEEN_SWITCH));
         QLabel* max_switch_time_label = new QLabel(tr("Max Time Between Shuffles: 60"));
         m_max_time_between_switch = new QSlider(Qt::Horizontal);
         m_max_time_between_switch->setRange(DEFAULT_MIN_SWITCH_TIME, DEFAULT_MAX_SWITCH_TIME);
-        m_max_time_between_switch->setValue(DEFAULT_MAX_SWITCH_TIME);
+        m_max_time_between_switch->setValue(Config::Get(Config::WIIMIX_MAX_TIME_BETWEEN_SWITCH));
         config_layout->addWidget(min_switch_time_label);
         config_layout->addWidget(m_min_time_between_switch);
         config_layout->addWidget(max_switch_time_label);
         config_layout->addWidget(m_max_time_between_switch);
         
+        // TODOx: add connect statements for the rest of these to update the config accordingly for persistence
+
         // Connect endless_mode to disable/enable number of switches
         connect(m_endless_mode, &QCheckBox::toggled, this, [this](bool checked) {
             m_num_switches->setDisabled(checked);
@@ -93,11 +114,13 @@ void WiiMixConfigWidget::CreateLayout(WiiMixEnums::Mode mode) {
     }
 
     // WiiMixEnums::Difficulty
-    QLabel* difficulty_label = new QLabel(tr("Objective WiiMixEnums::Difficulty:"));
+    QLabel* difficulty_label = new QLabel(tr("Objective Difficulty:"));
     m_difficulty = new QComboBox();
     for (int i = 0; i < static_cast<int>(WiiMixEnums::Difficulty::END); i++) {
         m_difficulty->addItem(WiiMixSettings::DifficultyToString(static_cast<WiiMixEnums::Difficulty>(i)));
     }
+    WiiMixEnums::Difficulty difficulty = Config::Get(Config::WIIMIX_DIFFICULTY);
+    m_difficulty->setCurrentIndex(static_cast<int>(difficulty));
     config_layout->addWidget(difficulty_label);
     config_layout->addWidget(m_difficulty);
 
@@ -112,6 +135,8 @@ void WiiMixConfigWidget::CreateLayout(WiiMixEnums::Mode mode) {
     for (int i = 0; i < static_cast<int>(WiiMixEnums::SaveStateBank::END); i++) {
         m_save_state_bank->addItem(WiiMixSettings::SaveStateBankToString(static_cast<WiiMixEnums::SaveStateBank>(i)));
     }
+    WiiMixEnums::SaveStateBank bank = Config::Get(Config::WIIMIX_SAVE_STATE_BANK);
+    m_save_state_bank->setCurrentIndex(static_cast<int>(bank));
     config_layout->addWidget(save_state_label);
     config_layout->addWidget(m_save_state_bank);
 
--- Source/Core/DolphinQt/WiiMix/TODO.md
+++ Source/Core/DolphinQt/WiiMix/TODO.md
@@ -6,7 +6,7 @@
 - [x] SettingsWindow.cpp LoadSettings()
 - [x] SettingsWindow.cpp SaveSettings()
 - [x] Get it to compile (QDir include issue...)
-
+- [] Make it so config state persists
 - [] Test some save and load configurations
 - [] Finish implementing shuffle (by Friday? Objectives...?)
 - [] Figure out how to run dolphins test cases/CI CD?
