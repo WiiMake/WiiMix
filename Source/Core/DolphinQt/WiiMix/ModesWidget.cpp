5a63e7361791357d8317cce706dbb4b3a8ea025c
Vladislav Pavlovskii <vdadvdad@gmail.com>
2024-11-24 20:41:13 -0500

    ScreenSaver initial implementation, cleaned up modesWidget by creating WiiMixButton class, initial implementation for tooltips (need to connect signals)

--- Source/Core/DolphinQt/WiiMix/ModesWidget.cpp
+++ Source/Core/DolphinQt/WiiMix/ModesWidget.cpp
@@ -33,7 +33,7 @@ void WiiMixModesWidget::CreateLayout() {
     for (int i = 0; i < static_cast<int>(WiiMixEnums::Mode::END); i++) {
         WiiMixButton* frame = new WiiMixButton();
         frame->setFixedSize(300, 169);
-        // Set up QFrame
+        // Set up WiiMixButton
         char * backgroundImagePath = (char *) malloc(200);
         strncpy(backgroundImagePath, (File::GetSysDirectory() + "Resources" + "/temporary_gradient_bg.png").data(), 200);
         frame->setBorderRadius(18);
@@ -41,7 +41,6 @@ void WiiMixModesWidget::CreateLayout() {
         frame->setBorderColor("gray");
         frame->setHasBackgroundImage(true);
         frame->setBackgroundImage(backgroundImagePath);
-        //frame->setToolTip(QString::fromStdString("Hello?"));
         frame->setCursor(Qt::PointingHandCursor);
         frame->installEventFilter(this); // To capture mouse events
         frame->setAttribute(Qt::WA_Hover, true);
@@ -76,7 +75,7 @@ void WiiMixModesWidget::CreateLayout() {
 }
 bool WiiMixModesWidget::eventFilter(QObject* obj, QEvent* event) {
     if ((event->type() == QEvent::HoverLeave)) {
-        QToolTip::hideText();
+        //QToolTip::hideText();
         for (int i = 0; i < static_cast<int>(WiiMixEnums::Mode::END); i++) {
             WiiMixButton* other_frame = (WiiMixButton*)(m_mode_layout->itemAt(i)->widget());
             if (other_frame) {
@@ -108,12 +107,25 @@ bool WiiMixModesWidget::eventFilter(QObject* obj, QEvent* event) {
             }
             else if (theme == "Clean Lite"){
                 shadow->setColor(Qt::white);
-
                 frame->setGraphicsEffect(shadow);
             }
             else if (theme == "Clean Pink"){
                 shadow->setColor(QColor(255, 192, 203));
                 frame->setGraphicsEffect(shadow);
+                std::string name = "Hello";
+                std::string description = "HelloHelloHello";
+                std::string icon = File::GetSysDirectory() + "Resources/Flag_Russia@2x.png";
+                std::ifstream t(File::GetSysDirectory() + "Resources/toolTip.html");
+                //std::cout << t.is_open() << "\n";
+                std::stringstream buffer;
+                buffer << t.rdbuf();
+                //std::cout << (buffer.str()) << "\n";
+                char toolTipString[1000];
+                snprintf(toolTipString, 999, buffer.str().data(), icon.data(), name.data(), description.data());
+                //std::cout << toolTipString << "\n";
+                //snprintf(toolTipString, 999, "<style> @keyframes appearance {0%% {opacity: 0} 60%% {opacity: 0.6} 100%% {opacity: 1}} .container{display: flex;} img {max-width: 25%%; max-height:15%%; float: left;} .text {font-size: 20px; padding-left: 20px; padding-top: 20%%; float: left; } .div {animation-duration: 5s; animation-timing-function: ease-in; animation-name: appearance}</style> <div class='container'> <div class='image'> <img src='%s'> </div> <div class='text'> <h2>%s</h2> </div> </div>\n%s", icon.data(), name.data(), description.data());
+
+                //QToolTip::showText(QCursor::pos(), QString::fromStdString(toolTipString), this, QRect(), 10000);
             }
             else {
                 qDebug("invalid theme name");
@@ -132,60 +144,25 @@ bool WiiMixModesWidget::eventFilter(QObject* obj, QEvent* event) {
                     other_frame->setBorderColor("gray"); // gray is border color for non-selected frame
                 }
             }
-            std::vector<BingoItem> b;
+            std::vector<BingoGame::BingoItem> b;
             b.reserve(25);
             for (int i = 0; i < 25; i++) {
-                BingoItem bItem;
+                BingoGame::BingoItem bItem;
                 bItem.name = "Hello";
                 bItem.description = "HelloHelloHello";
                 bItem.icon = "Flag_Russia@2x";
+                bItem.iconFull = File::GetSysDirectory() + "Resources/Flag_Russia@2x.png";
                 b.push_back(bItem);
             }
             BingoGame::createObjectiveWindow(b);
             std::string theme = Config::Get(Config::MAIN_THEME_NAME);
 
-            auto *shadow = new QGraphicsDropShadowEffect;
-            shadow->setBlurRadius(25);
-            shadow->setXOffset(0);
-            shadow->setYOffset(0);
-
             // Highlight the selected frame
-            if (theme == "Clean") {
-                shadow->setColor(Qt::black);
-                //auto *colorizeEffect = new QGraphicsColorizeEffect;
-                //colorizeEffect->setColor(Qt::black);
-                //frame->setGraphicsEffect(colorizeEffect);
-                frame->setBorderColor("black");
-            }
-            else if (theme == "Clean Blue") {
-                shadow->setColor(Qt::blue);
-                //auto *colorizeEffect = new QGraphicsColorizeEffect;
-                //colorizeEffect->setColor(Qt::blue);
-                //frame->setGraphicsEffect(colorizeEffect);
-                frame->setBorderColor("blue");
-            }
-            else if (theme == "Clean Emerald"){
-                shadow->setColor(Qt::green);
-                //auto *colorizeEffect = new QGraphicsColorizeEffect;
-                //colorizeEffect->setColor(Qt::green);
-                //frame->setGraphicsEffect(colorizeEffect);
-                frame->setBorderColor("green");
-            }
-            else if (theme == "Clean Lite"){
-                shadow->setColor(Qt::white);
-                //auto *colorizeEffect = new QGraphicsColorizeEffect;
-                //colorizeEffect->setColor(Qt::white);
-                //frame->setGraphicsEffect(colorizeEffect);
-                frame->setBorderColor("white");
-            }
-            else if (theme == "Clean Pink"){
-                shadow->setColor(QColor(255, 192, 203));
-                //auto *colorizeEffect = new QGraphicsColorizeEffect;
-                //colorizeEffect->setColor(QColor(255, 192, 203));
-                //frame->setGraphicsEffect(colorizeEffect);
-                frame->setBorderColor("rgb(255, 192, 203)");
-
-            }
+            if (theme == "Clean") { frame->setBorderColor("black"); }
+            else if (theme == "Clean Blue") { frame->setBorderColor("blue"); }
+            else if (theme == "Clean Emerald") { frame->setBorderColor("green"); }
+            else if (theme == "Clean Lite"){ frame->setBorderColor("white"); }
+            else if (theme == "Clean Pink"){ frame->setBorderColor("rgb(255, 192, 203)"); }
             else {
                 qDebug("invalid theme name, applying default theme instead");
                 frame->setStyleSheet(QStringLiteral("QFrame { border: 5px solid black; }"));
@@ -193,7 +170,6 @@ bool WiiMixModesWidget::eventFilter(QObject* obj, QEvent* event) {
             // Trigger configuration update for this WiiMixEnums::Mode
             WiiMixEnums::Mode selected_mode = static_cast<WiiMixEnums::Mode>(m_mode_layout->indexOf(frame));
             emit WiiMixModesWidget::ModeChanged(selected_mode);
-
             return true;
         }
     }
--- Source/Core/DolphinQt/WiiMix/ModesWidget.h
+++ Source/Core/DolphinQt/WiiMix/ModesWidget.h
@@ -11,6 +11,7 @@
 
 #include <array>
 #include "DolphinQt/WiiMix/Settings.h"
+#include "WiiMixButton.h"
 #include <iostream>
 
 
