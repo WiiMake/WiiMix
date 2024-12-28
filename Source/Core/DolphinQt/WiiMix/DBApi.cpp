899b05ea52c0560e9d44cbf20fbada72d247bb97
xanmankey <86136010+xanmankey@users.noreply.github.com>
2024-12-27 22:07:54 -0600

    updating build wiimix server only setting

--- Source/Core/DolphinQt/WiiMix/DBApi.cpp
+++
@@ -1,9 +0,0 @@
-// libpqxx: https://libpqxx.readthedocs.io/latest/getting-started.html
-
-#include "DolphinQt/WiiMix/DBApi.h"
-
-#include <iostream>
-
-
-// The server and client both use WiiMixDBApi to interact with the postgres database on the server
-WiiMixDBApi::WiiMixDBApi() {}
